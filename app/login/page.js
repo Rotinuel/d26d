cb18ca29975163cf9a77a10298b0a6bf8d5f197e
xalphi <emmanuelisagenius@gmail.com>
2026-04-20 18:56:04 +0100

    d2

--- app/login/page.js
+++ app/login/page.js
@@ -110,6 +110,7 @@
 
 
 "use client";
+export const dynamic = "force-dynamic";
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
